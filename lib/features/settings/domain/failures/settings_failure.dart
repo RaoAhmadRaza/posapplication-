1927251f0df12d96e28d05a408e6297e96174ac5
Muhammad Ahmad Raza <l1s22bscs0334@ucp.edu.pk>
2026-07-15 16:00:36 +0500

    feat(settings): typed SettingsFailure (sealed)

---
+++ lib/features/settings/domain/failures/settings_failure.dart
@@ -0,0 +1,25 @@
+sealed class SettingsFailure {
+  String get message;
+}
+
+class SettingsLoadFailure extends SettingsFailure {
+  @override
+  String get message => 'Could not load settings.';
+}
+
+class SettingsPermissionFailure extends SettingsFailure {
+  @override
+  String get message => 'You do not have permission to change settings.';
+}
+
+class SettingsNotFoundFailure extends SettingsFailure {
+  @override
+  String get message => 'The record could not be found.';
+}
+
+class SettingsUnknownFailure extends SettingsFailure {
+  final String details;
+  SettingsUnknownFailure(this.details);
+  @override
+  String get message => details;
+}
